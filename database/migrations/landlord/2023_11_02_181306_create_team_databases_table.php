92601ad8cea53d8712d0b1be910b3b6839768f55
= <inman.turbo@gmail.com>
2023-11-08 16:34:57 -0500

    fix typo

--- database/migrations/landlord/2023_11_02_181306_create_team_databases_table.php
+++ database/migrations/landlord/2023_11_02_181306_create_team_databases_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\TeamDatabaseType
+use App\Models\TeamDatabaseType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->uuid('uuid')->unique();
             $table->string('name');
-            $table->string('driver')->default(TeamDatabaseType:Mysql->name);
+            $table->string('driver')->default(TeamDatabaseType::Mysql->name);
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
