59b640392d353f0ce7e40dbbd983771e04e64add
= <inman.turbo@gmail.com>
2023-11-08 23:08:50 -0500

    restore orignal jetstream create team action

--- app/Actions/Jetstream/CreateTeam.php
+++ app/Actions/Jetstream/CreateTeam.php
@@ -23,30 +23,15 @@ class CreateTeam implements CreatesTeams
 
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
-            'team_database_id' => ['nullable', 'integer', 'exists:landlord.team_databases,id'],
         ])->validateWithBag('createTeam');
 
         AddingTeam::dispatch($user);
 
-        $teamData = [
+        $user->switchTeam($team = $user->ownedTeams()->create([
             'name' => $input['name'],
             'personal_team' => false,
-        ];
-
-        if (isset($input['team_database_id'])) {
-            $teamData['team_database_id'] = $input['team_database_id'];
-        }
-
-        $user->switchTeam($team = $user->ownedTeams()->create($teamData));
+        ]));
 
         return $team;
-    }
-
-    public function redirectTo(): string
-    {
-        // session()->flash('flash.banner', 'Team created successfully.');
-        // session()->flash('flash.bannerStyle', 'success');
-
-        return route('teams.show', ['team' => auth()->user()->currentTeam->id]);
     }
 }
