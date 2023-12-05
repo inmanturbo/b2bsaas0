90e4658ede7f50f703907f5d5a0e31c1bd16c41e
= <inman.turbo@gmail.com>
2023-12-05 16:41:35 -0500

    switch team by uuid in navigation form

--- resources/views/components/switchable-team.blade.php
+++ resources/views/components/switchable-team.blade.php
@@ -5,12 +5,12 @@
     @csrf
 
     <!-- Hidden Team ID -->
-    <input type="hidden" name="team_id" value="{{ $team->id }}">
+    <input type="hidden" name="team_uuid" value="{{ $team->uuid }}">
 
     <x-dynamic-component :component="$component" href="#" x-on:click.prevent="$root.submit();">
         <div class="flex items-center">
             @if (Auth::user()->isCurrentTeam($team))
-                <svg class="me-2 h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <svg class="w-5 h-5 text-green-400 me-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
             @endif
