b1d2fecc412185723c81a127c9b2e7233d2c4b65
= <inman.turbo@gmail.com>
2024-01-17 12:29:13 -0500

    format code

--- b2bsaas/src/Http/Middleware/TeamMiddleware.php
+++ b2bsaas/src/Http/Middleware/TeamMiddleware.php
@@ -2,11 +2,9 @@
 
 namespace B2bSaas\Http\Middleware;
 
-use App;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Schema;
 
 class TeamMiddleware
 {
@@ -20,31 +18,24 @@ class TeamMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        // if not running unit tests
+
         if (! app()->runningUnitTests()) {
 
             $team = $request->user()?->currentTeam;
-
-            // dd((new App\Models\Team)->getConnection(), (new App\Models\Team)->getConnectionName(), (new App\Models\Team)->getConnection()->getDatabaseName(), (new App\Models\Team)->getConnection()->getSchemaBuilder()->getConnection()->getDoctrineSchemaManager()->listTableNames());
 
             if (! $team) {
                 return $next($request);
             }
 
-            // migrate only once a day, cache a key to check if it has been done today
             if (! cache()->has('team_migrated_'.$team->id)) {
                 $team
                     ->migrate();
                 cache()->put('team_migrated_'.$team->id, true, now()->addDay());
             }
 
-            $team
-            // ->migrate()
-                ->configure()
-                ->use();
+            $team->configure()->use();
         }
 
-        // if app debug is true, log team authenticated
         if (config('app.debug')) {
             Log::debug('Team authenticated');
             session()->put('default_connection', config('database.default'));
