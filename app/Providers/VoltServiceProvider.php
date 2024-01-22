ff7296c9215a4126c132e9e327897df37a769842
= <inman.turbo@gmail.com>
2024-01-22 15:25:10 -0500

    format code

--- app/Providers/VoltServiceProvider.php
+++ app/Providers/VoltServiceProvider.php
@@ -4,11 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Inmanturbo\B2bSaas\MergesVoltMounts;
-use Livewire\Volt\Volt;
 
 class VoltServiceProvider extends ServiceProvider
 {
-
     use MergesVoltMounts;
 
     /**
@@ -24,7 +22,6 @@ class VoltServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
         $this->mergeVoltMounts([
             resource_path('views/livewire'),
             resource_path('views/pages'),
